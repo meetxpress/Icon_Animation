99b84eb74d2538052e26bc223bf5f894afa993ec
Meet Patel <pmeet7898@gmail.com>
2020-05-18 17:22:48 +0530

    done

--- app/src/main/java/com/example/icon_animation/DestinationActivity.kt
+++ app/src/main/java/com/example/icon_animation/DestinationActivity.kt
@@ -2,6 +2,7 @@ package com.example.icon_animation
 
 import android.content.Intent
 import android.graphics.drawable.Animatable
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -12,7 +13,7 @@ class DestinationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_destination)
-       // val drawable: Drawable = done.drawable
+        //val drawable: Drawable = done.drawable
         if (done.drawable is Animatable) {
             (done.drawable as Animatable).start()
             if(!(done.drawable as Animatable).isRunning){
