e450595320309828881c3e00acfb402d5f563a4d
Meet Patel <pmeet7898@gmail.com>
2020-05-18 15:03:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/icon_animation/DestinationActivity.kt
@@ -0,0 +1,25 @@
+package com.example.icon_animation
+
+import android.content.Intent
+import android.graphics.drawable.Animatable
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_destination.*
+
+class DestinationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination)
+       // val drawable: Drawable = done.drawable
+        if (done.drawable is Animatable) {
+            (done.drawable as Animatable).start()
+            if(!(done.drawable as Animatable).isRunning){
+                Handler().postDelayed({
+                    startActivity(Intent(this@DestinationActivity, MainActivity::class.java))
+                },1000)
+            }
+        }
+    }
+}
